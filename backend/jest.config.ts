d1c0b348da9eb4f9ef0ed74b129bf38d0318fafe
Lutful Haider <lutful.haider@neomore.fi>
2026-02-02 16:58:16 +0200

    Dockerize app and add test container

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,7 +1,10 @@
-/** @type {import('jest').Config} */
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
   clearMocks: true,
 };
+
+export default config;
